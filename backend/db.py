477e2ebde489195c8ced562d21d7ef537a99def2
Andrew Koss <kossa@umich.edu>
2025-11-24 16:44:04 -0500

    Try this?

--- backend/db.py
+++ backend/db.py
@@ -2,8 +2,11 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import psycopg2
+
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///app.db")
+print(DATABASE_URL)
 
 # Important for SQLite + threads
 connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
